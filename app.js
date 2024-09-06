1551627bd24d0b79cc9b981c687a06419a7115ba
afquidangen <alyssafayequidangen@gmail.com>
2024-09-06 20:21:19 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let age = 12;
+console.log(age);
+
+let score = 99;
+console.log(score)
+
+const pi = 3.14159
+console.log(pi)
+
+let isStudent = true;
+console.log(isStudent);
+
+let colors = ['red','green','yellow'];
+console.log(colors);
+
+
+//let = temporary
+//const = permanent
